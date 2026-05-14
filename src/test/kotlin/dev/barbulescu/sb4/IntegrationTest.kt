e427850f7963134d6c067081293820945a67e09d
Marius Barbulescu <marius.barbulescu@gmail.com>
2026-05-14 13:33:13 +0200

    use jdbc url for PostgreSQL TestContainers

--- src/test/kotlin/dev/barbulescu/sb4/IntegrationTest.kt
+++ src/test/kotlin/dev/barbulescu/sb4/IntegrationTest.kt
@@ -3,12 +3,10 @@ package dev.barbulescu.sb4
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.TestConstructor.AutowireMode.ALL
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestConstructor(autowireMode = ALL)
 @AutoConfigureRestTestClient
-@Import(TestcontainersConfiguration::class)
 annotation class IntegrationTest
